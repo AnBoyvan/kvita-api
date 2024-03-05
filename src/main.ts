1c87444ddf39e0aebf50ce244bb13eae72c848d8
AnBoyvan <andrew.boyvan1988@gmail.com>
2024-03-05 19:22:15 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import * as compression from 'compression';
 import * as cookieParser from 'cookie-parser';
-import helmet from 'helmet';
 
 import { AppModule } from './app.module';
 import { ErrorFilter } from './middlewares/error-filter.middleware';
@@ -20,18 +19,6 @@ async function bootstrap() {
     credentials: true,
     exposedHeaders: 'set-cookie',
   });
-  app.use(
-    helmet({
-      contentSecurityPolicy: {
-        directives: {
-          defaultSrc: [`'self'`],
-          styleSrc: [`'self'`, `'unsafe-inline'`],
-          imgSrc: [`'self'`, 'data:', 'validator.swagger.io'],
-          scriptSrc: [`'self'`, 'https:', `'unsafe-inline'`],
-        },
-      },
-    }),
-  );
   app.use(compression());
   app.useGlobalFilters(new ErrorFilter());
   await app.listen(port);
